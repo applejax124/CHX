33043fe45a20bd50437bfe8e735fb90994dc24a7
Doug Smith <dougsmith2015@gmail.com>
2017-03-18 00:34:40 -0400

    added 'google' command functionality

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,36 @@
-chrome.omnibox.onInputEntered.addListener(function(text){
-  alert(text);
+//Read omnibox input to parser
+chrome.omnibox.onInputEntered.addListener( function(text){
+  parse(text);
 });
+
+
+//function to parse commands
+//INPUT:
+//    str - raw text of the command
+//OUTPUT: 
+//  NONE - commands are executed in their own way
+function parse( str ){
+  args = str.split(" ");
+  switch ( args.shift() ){
+    case "google":
+      google_search(args);
+  }
+  
+}
+
+//function to launch a google search
+//INPUT: 
+//  params - an array of search terms
+function google_search( params ){
+  url="https://www.google.com/search?site=&source=hp&q=";
+  for (i in params){
+    url += params[i]+"+";
+  }
+  url += '\b';
+  navigate(url);
+}
+//function to change chrome's browser to specified window
+function navigate( loc ){
+  //sets current tab url to loc
+  chrome.tabs.update({"url":loc});
+}
--- scripts/content.js
+++
@@ -1,8 +0,0 @@
-//console.log("CONTENT loaded");
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log("TAB update");
-  chrome.tabs.getCurrent(function(tab){
-    console.log(tab);
-  });
-});
