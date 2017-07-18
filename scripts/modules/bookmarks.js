3b85b1eecc01b981f7e27638a05059eb40ddb741
Doug Smith <dougsmith2015@gmail.com>
2017-07-17 22:41:57 -0400

    Added bookmarks module

---
+++ scripts/modules/bookmarks.js
@@ -0,0 +1,21 @@
+/*
+* Bookmakrs Utility
+* Douglas J. Smith
+* 7-17-2017
+*/
+
+addModule(bookmarksUtility,
+[
+  "bkmrk",
+  "bkmrks",
+  "bookmark",
+  "bookmarks"
+]);
+
+// Coordinates bookmark operation.
+function bookmarksUtility(params){
+  // If no parameters, then just open bookmarks. Else try commands.
+  if(params.length < 1){
+    navigate("chrome://bookmarks","");
+  }
+}
