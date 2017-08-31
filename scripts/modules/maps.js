9ae4526f535edd395be35c47ca721179fcb6728a
Doug Smith <dougsmith2015@gmail.com>
2017-08-31 01:46:12 -0400

    Added maps module (just navigation functionality)

---
+++ scripts/modules/maps.js
@@ -0,0 +1,14 @@
+/*
+* Google Maps utility
+* Douglas J. Smith
+* 08-31-2017
+*/
+addModule(launchDrive,
+[
+  "maps"
+]);
+
+// launches drive application
+function launchDrive(params){
+  navigate("https://www.google.com/maps/");
+}
