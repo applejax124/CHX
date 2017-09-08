d5d3dfbc6079dcb5ef6e43bf58ba154bdfa0fe52
Doug Smith <dougsmith2015@gmail.com>
2017-09-07 22:07:50 -0400

    GMAIL: Added fragment addition feature in interpretation command. Chromex can now find #inbox, #all, etc.

--- scripts/modules/google-webapps/mail/gmail.js
+++ scripts/modules/google-webapps/mail/gmail.js
@@ -12,5 +12,19 @@ addModule(launchGmail,
 // launches gmail
 function launchGmail(params){
   navigateGoogleWebapp("https://mail.google.com/mail/u/", params);
+  
+  targetUrl="https://mail.google.com/mail/u/";
+  if (params.length>0) {
+    // App number is always first.
+    navigate(targetUrl+params.shift()+"/");
+    while (params.length>0){
+      // Command to gmail app.
+      com = params.shift();
+      navigate(targetUrl+"#"+com);
+    }
+  } else {
+    navigate(targetUrl+"0/");
+  }
+
 }
 
